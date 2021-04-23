64fb0ade5b47a530f4775c0f7c78ee2eb17c67cf
Denis <denisabsdenis@yandex.ru>
2021-04-23 17:02:33 +0300

    Add employee model

---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmployeeDirectory.Models
+{
+    /// <summary>
+    /// Represents an employee
+    /// </summary>
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime BirthDay { get; set; }
+    }
+}
