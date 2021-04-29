0018ade2cf16a0f49c6103f6a544a961154b9dd7
Denis <denisabsdenis@yandex.ru>
2021-04-29 15:07:56 +0300

    Add HintTextBox element like TextBox element only with hint

---
+++ ClientApp/Elements/HintTextBox.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientApp.Elements
+{
+    /// <summary>
+    /// Interaction logic for PromptTextBox.xaml
+    /// </summary>
+    public partial class HintTextBox : Grid
+    {
+        public string Prompt { set { promptBlock.Text = value; } }
+        public string Text { get { return txtUserEntry.Text; } }
+
+        public HintTextBox()
+        {
+            InitializeComponent();
+        }
+
+        private void txtUserEntry_KeyUp(object sender, KeyEventArgs e)
+        {
+            promptBlock.Visibility = (txtUserEntry.Text.Length == 0) ? Visibility.Visible : Visibility.Hidden;
+        }
+    }
+}
