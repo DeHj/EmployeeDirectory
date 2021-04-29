78b1dcec414aaa3c5dec8eb3d0418bfcd3291315
Denis <denisabsdenis@yandex.ru>
2021-04-29 15:27:20 +0300

    Add xaml element EmployeeField for displaying information about one employee

---
+++ ClientApp/Elements/EmployeeField.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientApp.Elements
+{
+    /// <summary>
+    /// Interaction logic for EmployeeField.xaml
+    /// </summary>
+    public partial class EmployeeField : Grid
+    {
+        public EmployeeField(string firstName, string secondName, string middleName, string login, DateTime? birthday)
+        {
+            InitializeComponent();
+
+            Name.Text = $"{firstName} {secondName ?? ""} {middleName ?? ""}".Replace("  ", " ");
+            Login.Text = login;
+            if (birthday != null) Birthday.Text = birthday?.GetDateTimeFormats('D').First();
+        }
+    }
+}
