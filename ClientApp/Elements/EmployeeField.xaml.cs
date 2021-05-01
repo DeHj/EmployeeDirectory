15b2c253487e726586fbc778e8e544315fe91f7f
Denis <denisabsdenis@yandex.ru>
2021-05-01 11:42:33 +0300

    Add a ctor accepting Employee instance

--- ClientApp/Elements/EmployeeField.xaml.cs
+++ ClientApp/Elements/EmployeeField.xaml.cs
@@ -18,7 +18,7 @@ namespace ClientApp.Elements
     /// <summary>
     /// Interaction logic for EmployeeField.xaml
     /// </summary>
-    public partial class EmployeeField : Grid
+    public partial class EmployeeField : Border
     {
         public EmployeeField(string firstName, string secondName, string middleName, string login, DateTime? birthday)
         {
@@ -27,6 +27,15 @@ namespace ClientApp.Elements
             nameText.Text = $"{firstName} {secondName ?? ""} {middleName ?? ""}".Replace("  ", " ");
             loginText.Text = login;
             if (birthday != null) birthdayText.Text = birthday?.GetDateTimeFormats('D').First();
+        }
+
+        public EmployeeField(EmployeeDirectory.Models.Employee employee)
+        {
+            InitializeComponent();
+
+            nameText.Text = $"{employee.FirstName} {employee.SecondName?? ""} {employee.MiddleName?? ""}".Replace("  ", " ");
+            loginText.Text = employee.Login;
+            if (employee.BirthDay != null) birthdayText.Text = employee.BirthDay?.GetDateTimeFormats('D').First();
         }
     }
 }
