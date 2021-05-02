cfc35db6135226c833bbfa8110738c6cb7463de9
Denis <denisabsdenis@yandex.ru>
2021-05-02 23:04:36 +0300

    Fix sql queries

--- SqlQueries/stored procedures (employees).sql
+++ SqlQueries/stored procedures (employees).sql
@@ -1,6 +1,6 @@
 use EmployeesDirectory
 
-/*
+
 DROP PROCEDURE dbo.get_all_employees;
 GO
 CREATE PROCEDURE get_all_employees
@@ -10,11 +10,11 @@ AS
 BEGIN
 	SELECT login, first_name, second_name, middle_name, birthday, id from Employees
 	ORDER BY id
-	OFFSET @from - 1 ROW FETCH NEXT @page_size ROW ONLY
+	OFFSET @from ROW FETCH NEXT @page_size ROW ONLY
 END
-*/
 
-/*
+
+
 DROP PROCEDURE dbo.get_employees_by_name;
 GO
 CREATE PROCEDURE get_employees_by_name
@@ -28,14 +28,14 @@ BEGIN
 	SELECT login, first_name, second_name, middle_name, birthday, id from Employees
 	WHERE
 		first_name LIKE @first_name
-		AND second_name LIKE @second_name
-		AND middle_name LIKE @middle_name
+		AND ((second_name IS NULL AND @second_name = '%') OR second_name LIKE @second_name)
+		AND ((middle_name IS NULL AND @middle_name = '%') OR middle_name LIKE @middle_name)
 	ORDER BY id
-	OFFSET @from - 1 ROW FETCH NEXT @page_size ROW ONLY
+	OFFSET @from ROW FETCH NEXT @page_size ROW ONLY
 END;
-*/
 
-/*
+
+
 DROP PROCEDURE dbo.get_phones_by_id;
 GO
 CREATE PROCEDURE get_phones_by_id
@@ -45,4 +45,3 @@ BEGIN
 	SELECT phone_number, id_employee from Phones
 	WHERE id_employee = @id_employee
 END;
-*/
