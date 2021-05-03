67b2d704ce401d70170497965fb9994c77d98674
Denis <denisabsdenis@yandex.ru>
2021-05-03 08:02:02 +0300

    Make public some inner members in Employee model

--- Models/Models/Employee.cs
+++ Models/Models/Employee.cs
@@ -15,32 +15,31 @@ namespace EmployeeDirectory.Models
         public DateTime? BirthDay { get; set; }
 
 
-        private const string invalidSymbols = ".-~:/?#[]@!$#&'()*+,;=%";
+        public const string InvalidSymbols = ".-~:/?#[]@!$#&'()*+,;=%";
+
+        public static bool StringIsValid(string s)
+        {
+            foreach (var ch in s)
+            {
+                if (InvalidSymbols.Contains(ch))
+                    return false;
+            }
+            return true;
+        }
 
         /// <summary>
         /// Check model on the validity
         /// </summary>
         public bool IsValid()
         {
-            static bool isValid(string s)
-            {
-                foreach (var ch in s)
-                {
-                    if (invalidSymbols.Contains(ch))
-                        return false;
-                }
-                return true;
-            }
-
-
-            if (FirstName.Length == 0 || isValid(Login) == false)
+            if (FirstName.Length == 0 || StringIsValid(Login) == false)
                 return false;
-            if (FirstName.Length == 0 || isValid(FirstName) == false)
+            if (FirstName.Length == 0 || StringIsValid(FirstName) == false)
                 return false;
 
-            if (SecondName != null && isValid(SecondName) == false)
+            if (SecondName != null && StringIsValid(SecondName) == false)
                 return false;
-            if (MiddleName != null && isValid(MiddleName) == false)
+            if (MiddleName != null && StringIsValid(MiddleName) == false)
                 return false;
 
             return true;
