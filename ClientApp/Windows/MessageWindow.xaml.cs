1d93f0aaff5da68e172dd7a02a7fa6f071afd0e0
Denis <denisabsdenis@yandex.ru>
2021-05-03 09:15:17 +0300

    Add MessageWindow window

---
+++ ClientApp/Windows/MessageWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ClientApp.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ConfirmDialog.xaml
+    /// </summary>
+    public partial class MessageWindow : Window
+    {
+        public MessageWindow(string message)
+        {
+            InitializeComponent();
+            MessageTextBlock.Text = message;
+        }
+
+        private void accept_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+            //this.DialogResult = true;
+        }
+    }
+}
